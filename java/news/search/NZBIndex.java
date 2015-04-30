5803656a06ba5ba41913f5c4d93782c1d14ff79b
edurocher <edurocher0@gmail.com>
2015-04-30 11:13:04 +0200

    V 4.5.5 Small layout fixes in heading

--- java/news/search/NZBIndex.java
+++ java/news/search/NZBIndex.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.zip.GZIPInputStream;
 
