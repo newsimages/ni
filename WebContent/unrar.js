be9a3f1690d34c176a344682ab331a2dc002cb8e
edurocher <edurocher0@gmail.com>
2016-10-26 15:40:23 +0200

    Sort files using AlphanumComparator algorithm.

--- WebContent/unrar.js
+++ WebContent/unrar.js
@@ -15,9 +15,45 @@ define(["dojo/Deferred", "libunrar.js"], function(Deferred, readRARContent) {
 		return attachments;
 	}
 
+	/* ********************************************************************
+	 * Alphanum sort() function version - case sensitive
+	 *  - Slower, but easier to modify for arrays of objects which contain
+	 *    string properties
+	 *
+	 */
+	function alphanum(a, b) {
+	  function chunkify(t) {
+	    var tz = new Array();
+	    var x = 0, y = -1, n = 0, i, j;
+
+	    while (i = (j = t.charAt(x++)).charCodeAt(0)) {
+	      var m = (i == 46 || (i >=48 && i <= 57));
+	      if (m !== n) {
+	        tz[++y] = "";
+	        n = m;
+	      }
+	      tz[y] += j;
+	    }
+	    return tz;
+	  }
+
+	  var aa = chunkify(a);
+	  var bb = chunkify(b);
+
+	  for (var x = 0; aa[x] && bb[x]; x++) {
+	    if (aa[x] !== bb[x]) {
+	      var c = Number(aa[x]), d = Number(bb[x]);
+	      if (c == aa[x] && d == bb[x]) {
+	        return c - d;
+	      } else return (aa[x] > bb[x]) ? 1 : -1;
+	    }
+	  }
+	  return aa.length - bb.length;
+	}
+	
 	function dir(d, attachments){
 		_dir(d, attachments);
-		attachments.sort(function(a1, a2) { return a1.filename.localeCompare(a2.filename); });
+		attachments.sort(function(a1, a2) { return alphanum(a1.filename, a2.filename); });
 		return attachments;
 	}
 	
